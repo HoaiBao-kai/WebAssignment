68a4f5027745c72984686b8375211cfdd9cf8ff9
hoaibao.kai <phamhoaibao01052001@gmail.com>
2022-01-04 11:07:42 +0700

    database

--- mysql/sql/data.sql
+++ mysql/sql/data.sql
@@ -1,15 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 04, 2022 lúc 05:06 AM
+-- Phiên bản máy phục vụ: 10.4.22-MariaDB
+-- Phiên bản PHP: 8.1.1
 
-CREATE DATABASE IF NOT EXISTS `product_management` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `product_management`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE `product` (
-  `id` int PRIMARY KEY auto_increment NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `price` int DEFAULT 0,
-  `description` varchar(255) DEFAULT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO `product` (`name`, `price`, `description`) VALUES
-('Macbook Pro', 1500, '16 inch, 32GB RAM'),
-('iPhone X', 1100, 'No Adapter');
+--
+-- Cơ sở dữ liệu: `webassingment_database`
+--
+CREATE DATABASE IF NOT EXISTS `webassingment_database` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `webassingment_database`;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `account`
+--
+
+CREATE TABLE `account` (
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `fullname` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `hash_password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `possition` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `department` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `avatar` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `account`
+--
+
+INSERT INTO `account` (`username`, `fullname`, `hash_password`, `possition`, `department`, `avatar`) VALUES
+('admin', 'Phan Van An', '$2a$12$EPloFHUnsX3G0lhjFJO11OFSzTfJqTOembcWAKUwFih', 'admin', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `department`
+--
+
+CREATE TABLE `department` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `room` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `detail` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `file`
+--
+
+CREATE TABLE `file` (
+  `file_tag` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `link` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  `detail` varchar(5000) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `task`
+--
+
+CREATE TABLE `task` (
+  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `detail` varchar(5000) COLLATE utf8_unicode_ci NOT NULL,
+  `start_day` datetime NOT NULL,
+  `deadline` datetime NOT NULL,
+  `account_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tag_file` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Chỉ mục cho bảng `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
