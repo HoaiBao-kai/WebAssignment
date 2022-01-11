ec693acf5542e9881edd5002b44ff6bb1695df71
hoaibao.kai <phamhoaibao01052001@gmail.com>
2022-01-11 22:28:12 +0700

    update 22222

--- www/views/adddepartment.php
+++ www/views/adddepartment.php
@@ -1,94 +1,115 @@
-<?php 
-    session_start();
-    if (!isset($_SESSION['user'])) {
-        header('Location: ../views/login.php');
-        exit();
-    }
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: ../views/login.php');
+    exit();
+}
 
-    require_once('../admin/db.php');
+require_once('../admin/db.php');
 ?>
+<?php
 
+$error = '';
+$id = '';
+$name = '';
+$room = '';
+$detail = '';
+if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['room']) && isset($_POST['detail'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $room = $_POST['room'];
+    $detail = $_POST['detail'];
+
+
+    if (empty($id)) {
+        $error = 'Hãy nhập mã phòng ban';
+    } else if (empty($name)) {
+        $error = 'Hãy nhập tên phòng ban';
+    } else if (empty($room)) {
+        $error = 'Hãy nhập số phòng ban';
+    } else if (empty($detail)) {
+        $error = 'Hãy nhập mô tả phòng ban';
+    } else {
+        add_department($id, $name, $room, $detail);
+        header('Location: ../views/department_management.php');
+    }
+}
+?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <title>New department</title>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
-    <script src="../main.js"></script>
-  </head>
-  <body>
-    <?php
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
 
-        $error = '';
-        $id = '';
-        $name = '';
-        $room = '';
-        $detail = '';
-
-
-        
-        if (isset($_POST['id']) && isset($_POST['name']) && isset($_POST['room']) && isset($_POST['detail']))
-        {
-            $id = $_POST['id'];
-            $name = $_POST['name'];
-            $room = $_POST['room'];
-            $detail = $_POST['detail'];
-
-
-            if (empty($id)) {
-                $error = 'Hãy nhập mã phòng ban';
-            }
-            else if (empty($name)) {
-                $error = 'Hãy nhập tên phòng ban';
-            }
-            else if (empty($room)) {
-                $error = 'Hãy nhập số phòng ban';
-            }
-            else if (empty($detail)) {
-                $error = 'Hãy nhập mô tả phòng ban';
-            }
-            else {
-                add_department($id, $name, $room, $detail);
-                header('Location: ../views/department_management.php');
-            }
-        }
-    
-    ?>
+<body>
+    <div class="w3-bar w3-light-grey w3-border w3-large">
+        <div class="container">
+            <a href="leader_index.php" class="w3-bar-item w3-button"><i class="fas fa-house-user" style="font-size: 30px;"></i></a>
+            <a class="navbar-brand" href="#"><strong><?= $_SESSION['fullname'] ?></strong></a>
+            <div class="w3-dropdown-hover" style="float: right;">
+                <a href="#" class="w3-bar-item w3-button"><i class="fas fa-user-alt" style="font-size: 30px;"></i></a>
+                <!-- <a class="btn btn-default" href="#"><strong><?php echo $_SESSION['fullname'] ?></strong></a> -->
+                <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 50px;">
+                    <a href="../views/employeeprofile.php?username=<?= $user_id ?>" class="w3-bar-item w3-button">Thông tin cá nhân</a>
+                    <a href="../views/resetpassword.php" class="w3-bar-item w3-button">Đổi mật khẩu</a>
+                    <a href="../views/logout.php" class="w3-bar-item w3-button">Đăng xuất</a>
+                </div>
+            </div>
+            <div class="w3-dropdown-hover" style="float: right;">
+                <a href="#" class="w3-bar-item w3-button"><i class="fas fa-address-card" style="font-size: 30px;"></i></a>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4" style="margin-top: 50px;">
+                    <a class="w3-bar-item w3-button" href="../views/dayoff_management.php">Quản lý ngày nghỉ</a>
+                    <a class="w3-bar-item w3-button" href="addtask.php">Quản lý nhiệm vụ</a>
+                    <a class="w3-bar-item w3-button" href="../views/employee_dayoff.php">Ngày nghỉ phép</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6 col-lg-5">
                 <h3 class="text-center text-secondary mt-5 mb-3">Thêm phòng ban</h3>
                 <form method="post" action="" class="border rounded w-100 mb-5 mx-auto px-3 pt-3 bg-light">
-                    <div class="form-group">
-                        <label>Mã phòng ban</label>
-                        <input class ="form-control " name="id" id="id" type="text" placeholder="Mã phòng ban">
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label>Mã phòng ban</label>
+                            <input class="form-control " name="id" id="id" type="text" placeholder="Mã phòng ban">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label>Số phòng</label>
+                            <input class="form-control" name="room" id="room" type="text" placeholder="Nhập số phòng">
+                        </div>
                     </div>
+
                     <div class="form-group">
                         <label>Tên phòng ban</label>
-                        <input class ="form-control" name="name" id="name" type="text" placeholder="Tên phòng ban">
+                        <input class="form-control" name="name" id="name" type="text" placeholder="Tên phòng ban">
                     </div>
-                    <div class="form-group">
-                        <label>Số phòng</label>
-                        <input class ="form-control" name="room" id="room" type="text" placeholder="Nhập số phòng">
-                    </div>
+
                     <div class="form-group">
                         <label>Mô tả</label>
-                        <textarea  class ="form-control"name="detail" id="detail" cols="20" rows="10" style="height:100px" placeholder="Thêm mô tả về phòng ban"></textarea>
+                        <textarea class="form-control" name="detail" id="detail" cols="20" rows="10" style="height:100px" placeholder="Thêm mô tả về phòng ban"></textarea>
                     </div>
                     <div class="form-group">
-                        <p class="text-center"style="margin:15px">
+                        <p class="text-center" style="margin:15px">
                             <?php
-                                if (!empty($error)) {
-                                    echo "<div class='alert alert-danger'>$error</div>";
-                                }
+                            if (!empty($error)) {
+                                echo "<div class='alert alert-danger'>$error</div>";
+                            }
                             ?>
                             <button type="submit" class="btn btn-success px-5 h-5">Thêm</button></span>
                             <a href="../views/department_management.php" class="btn btn-danger px-5 h-5">Huỷ bỏ</a></span>
-                        </p> 
+                        </p>
                     </div>
                 </form>
             </div>
@@ -99,5 +120,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
